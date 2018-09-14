19cb993ee9dce76146456cb895ec51919d58913b
Mads R. Havmand <mhv@energinet.dk>
2018-09-14 13:36:51 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.2.1',
+    version='1.2.2',
 
     description='''CKAN extension for Energinet''',
     long_description=long_description,
